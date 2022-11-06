37344929812eeb6134a1bb1254400acf78a6e61b
oozaw <79121990+oozaw@users.noreply.github.com>
2022-11-06 13:40:08 +0700

    update dashboard components color

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                <div class="col-lg-3 col-6">
                   <!-- small box -->
-                  <div class="small-box bg-info">
+                  <div class="small-box bg-purple">
                      <div class="inner">
                         <h3>{{ $kelas->count() }}</h3>
 
@@ -71,7 +71,7 @@
                @cannot('guru')
                   <div class="col-lg-3 col-6">
                      <!-- small box -->
-                     <div class="small-box bg-warning">
+                     <div class="small-box" style="background-color: #C52E3C">
                         <div class="inner text-light">
                            <h3>{{ $pekerjas->where('jabatan', 'Guru')->count() }}</h3>
 
@@ -80,14 +80,14 @@
                         <div class="icon">
                            <i class="fas fa-chalkboard-teacher"></i>
                         </div>
-                        <a href="/guru" class="small-box-footer" style="color: #FFFFFF !important;">Selengkapnya <i
-                              class="fas fa-arrow-circle-right"></i></a>
+                        <a href="/guru" class="small-box-footer text-white" style="color: #FFFFFF !important;">Selengkapnya
+                           <i class="fas fa-arrow-circle-right"></i></a>
                      </div>
                   </div>
                   <!-- ./col -->
                   <div class="col-lg-3 col-6">
                      <!-- small box -->
-                     <div class="small-box bg-danger">
+                     <div class="small-box text-white" style="background-color: #AF2733">
                         <div class="inner">
                            <h3>{{ $pekerjas->where('jabatan', 'Staf Tata Usaha')->count() }}</h3>
 
@@ -96,14 +96,14 @@
                         <div class="icon">
                            <i class="fas fa-users-cog"></i>
                         </div>
-                        <a href="/tata-usaha" class="small-box-footer">Selengkapnya <i
+                        <a href="/tata-usaha" class="small-box-footer text-white">Selengkapnya <i
                               class="fas fa-arrow-circle-right"></i></a>
                      </div>
                   </div>
                   <!-- ./col -->
                   <div class="col-lg-3 col-6">
                      <!-- small box -->
-                     <div class="small-box bg-secondary">
+                     <div class="small-box text-white" style="background-color: #98202A">
                         <div class="inner">
                            <h3>{{ $pekerjas->where('jabatan', 'Staf Lainnya')->count() }}</h3>
 
@@ -112,14 +112,14 @@
                         <div class="icon">
                            <i class="fas fa-user-friends"></i>
                         </div>
-                        <a href="/staf-lain" class="small-box-footer">Selengkapnya <i
+                        <a href="/staf-lain" class="small-box-footer text-white">Selengkapnya <i
                               class="fas fa-arrow-circle-right"></i></a>
                      </div>
                   </div>
                   <!-- ./col -->
                   <div class="col-lg-3 col-6">
                      <!-- small box -->
-                     <div class="small-box bg-maroon">
+                     <div class="small-box" style="background-color: #F1E0C5">
                         <div class="inner">
                            <h3>{{ $surat_masuk->count() }}</h3>
 
@@ -128,14 +128,14 @@
                         <div class="icon">
                            <i class="fas fa-inbox-in"></i>
                         </div>
-                        <a href="/surat-masuk" class="small-box-footer">Selengkapnya <i
+                        <a href="/surat-masuk" class="small-box-footer text-black" style="color: #000000">Selengkapnya <i
                               class="fas fa-arrow-circle-right"></i></a>
                      </div>
                   </div>
                   <!-- ./col -->
                   <div class="col-lg-3 col-6">
                      <!-- small box -->
-                     <div class="small-box bg-purple">
+                     <div class="small-box" style="background-color: #17BEBB">
                         <div class="inner">
                            <h3>{{ $surat_keluar->count() }}</h3>
 
@@ -144,7 +144,7 @@
                         <div class="icon">
                            <i class="fas fa-inbox-out"></i>
                         </div>
-                        <a href="/surat-keluar" class="small-box-footer">Selengkapnya <i
+                        <a href="/surat-keluar" class="small-box-footer" style="color: #000000">Selengkapnya <i
                               class="fas fa-arrow-circle-right"></i></a>
                      </div>
                   </div>
@@ -153,7 +153,7 @@
                <div class="col-md-6">
                   <div class="card card-success">
                      <div class="card-header">
-                        <h3 class="card-title">Siswa Chart</h3>
+                        <h3 class="card-title">Perbandingan Jumlah Siswa Laki-laki dan Perempuan</h3>
                         <div class="card-tools">
                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
                               <i class="fas fa-minus"></i>
@@ -181,9 +181,9 @@
                   </div>
                </div>
                <div class="col-md-6">
-                  <div class="card card-primary">
-                     <div class="card-header">
-                        <h3 class="card-title">Pegawai Chart</h3>
+                  <div class="card">
+                     <div class="card-header text-white" style="background-color: #C52E3C">
+                        <h3 class="card-title">Perbandingan Jumlah Jabatan Pegawai</h3>
                         <div class="card-tools">
                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
                               <i class="fas fa-minus"></i>
@@ -217,7 +217,7 @@
                      <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title mt-1">
                            <i class="fas fa-chart-line mr-1"></i>
-                           Grafik Jumlah Keseluruhan Siswa
+                           Grafik Jumlah Siswa Per Tahun Pelajaran
                         </h3>
                         <div class="card-tools">
                            <button type="button" class="btn bg-success btn-sm" data-card-widget="collapse">
@@ -241,14 +241,15 @@
                   </div>
                </div>
                <div class="col-md-6">
-                  <div class="card bg-info">
+                  {{-- <div class="card" style="background-color: #12822E"> --}}
+                  <div class="card bg-success">
                      <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
-                        <h3 class="card-title mt-1">
+                        <h3 class="card-title mt-1 text-white">
                            <i class="fas fa-chart-line mr-1"></i>
-                           Grafik Jumlah Siswa Baru
+                           Grafik Jumlah Siswa Baru Per Tahun Pelajaran
                         </h3>
                         <div class="card-tools">
-                           <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
+                           <button type="button" class="btn btn-sm bg-success" data-card-widget="collapse">
                               <i class="fas fa-minus"></i>
                            </button>
                         </div>
@@ -269,14 +270,15 @@
                   </div>
                </div>
                <div class="col-md-6">
-                  <div class="card bg-warning">
+                  {{-- <div class="card" style="background-color: #16C21C"> --}}
+                  <div class="card bg-success">
                      <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title mt-1">
                            <i class="fas fa-chart-line mr-1"></i>
-                           Grafik Jumlah Siswa Keluar/Pindah
+                           Grafik Jumlah Siswa Keluar/Pindah Per Tahun Pelajaran
                         </h3>
                         <div class="card-tools">
-                           <button type="button" class="btn bg-warning btn-sm" data-card-widget="collapse">
+                           <button type="button" class="btn btn-sm bg-success" data-card-widget="collapse">
                               <i class="fas fa-minus"></i>
                            </button>
                         </div>
@@ -297,15 +299,14 @@
                   </div>
                </div>
                <div class="col-md-6">
-                  <div class="card" style="background-color: #F39237 ">
+                  <div class="card bg-primary">
                      <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title mt-1">
                            <i class="fas fa-chart-line mr-1"></i>
-                           Grafik Jumlah Prestasi
+                           Grafik Jumlah Prestasi Per Tahun Pelajaran
                         </h3>
                         <div class="card-tools">
-                           <button type="button" class="btn btn-sm" data-card-widget="collapse"
-                              style="background-color: #F39237">
+                           <button type="button" class="btn btn-sm bg-primary" data-card-widget="collapse">
                               <i class="fas fa-minus"></i>
                            </button>
                         </div>
@@ -330,11 +331,11 @@
                      <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title mt-1 text-white">
                            <i class="fas fa-chart-line mr-1"></i>
-                           Grafik Jumlah Pegawai
+                           Grafik Jumlah Pegawai Per Tahun Pelajaran
                         </h3>
                         <div class="card-tools">
-                           <button type="button" class="btn btn-sm text-white" data-card-widget="collapse"
-                              style="background-color: #B10F2E">
+                           <button type="button" class="btn btn-sm bg-red" data-card-widget="collapse"
+                              style="background-color: #B10F2E !important">
                               <i class="fas fa-minus"></i>
                            </button>
                         </div>
@@ -359,10 +360,10 @@
                      <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
                         <h3 class="card-title">
                            <i class="fas fa-chart-bar mr-1"></i>
-                           Bar Jumlah Surat
+                           Perbandingan Jumlah Surat Per Tahun Pelajaran
                         </h3>
                         <div class="card-tools">
-                           <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                           <button type="button" class="btn btn-sm bg-dark" data-card-widget="collapse">
                               <i class="fas fa-minus"></i>
                            </button>
                         </div>
@@ -382,7 +383,6 @@
                               width="820" height="500"></canvas>
                         </div>
                      </div>
-
                   </div>
                </div>
             </div>
@@ -421,7 +421,7 @@
             ],
             datasets: [{
                data: [$("#laki-laki").text(), $("#perempuan").text()],
-               backgroundColor: ['#093eeb', '#09de33'],
+               backgroundColor: ['#093eeb', '#E936A4'],
             }]
          }
          var donutOptions = {
@@ -619,11 +619,11 @@
                borderWidth: 2,
                lineTension: 0,
                spanGaps: true,
-               borderColor: '#0E0F19',
+               borderColor: '#efefef',
                pointRadius: 3,
                pointHoverRadius: 7,
-               pointColor: '#0E0F19',
-               pointBackgroundColor: '#0E0F19',
+               pointColor: '#efefef',
+               pointBackgroundColor: '#efefef',
                data: [
                   @foreach ($tahun_ajaran->sortBy('tahun_ajaran') as $ta)
                      {{ $ta->jml_siswa_keluar }},
@@ -641,22 +641,22 @@
             scales: {
                xAxes: [{
                   ticks: {
-                     fontColor: '#0E0F19'
+                     fontColor: '#efefef'
                   },
                   gridLines: {
                      display: false,
-                     color: '#0E0F19',
+                     color: '#efefef',
                      drawBorder: false
                   }
                }],
                yAxes: [{
                   ticks: {
                      stepSize: 2,
-                     fontColor: '#0E0F19'
+                     fontColor: '#efefef'
                   },
                   gridLines: {
                      display: true,
-                     color: '#0E0F19',
+                     color: '#efefef',
                      drawBorder: false
                   }
                }]
@@ -688,11 +688,11 @@
                borderWidth: 2,
                lineTension: 0,
                spanGaps: true,
-               borderColor: '#0E0F19',
+               borderColor: '#FFFFFF',
                pointRadius: 3,
                pointHoverRadius: 7,
-               pointColor: '#0E0F19',
-               pointBackgroundColor: '#0E0F19',
+               pointColor: '#FFFFFF',
+               pointBackgroundColor: '#FFFFFF',
                data: [
                   @foreach ($tahun_ajaran->sortBy('tahun_ajaran') as $ta)
                      {{ $ta->jml_prestasi }},
@@ -710,22 +710,22 @@
             scales: {
                xAxes: [{
                   ticks: {
-                     fontColor: '#0E0F19'
+                     fontColor: '#FFFFFF'
                   },
                   gridLines: {
                      display: false,
-                     color: '#0E0F19',
+                     color: '#FFFFFF',
                      drawBorder: false
                   }
                }],
                yAxes: [{
                   ticks: {
                      stepSize: 5,
-                     fontColor: '#0E0F19'
+                     fontColor: '#FFFFFF'
                   },
                   gridLines: {
                      display: true,
-                     color: '#0E0F19',
+                     color: '#FFFFFF',
                      drawBorder: false
                   }
                }]
